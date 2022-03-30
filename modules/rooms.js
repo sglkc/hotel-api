58526593fb818b031d946b9fc96f94d447c611c9
sglkc <31957516+sglkc@users.noreply.github.com>
2022-03-30 10:44:41 +0700

    chore: translate to English

--- modules/rooms.js
+++ modules/rooms.js
@@ -3,9 +3,9 @@ const { mysql } = require('../db/connection.js');
 function getRooms(req, res) {
   let query =
     `
-    SELECT jk.name AS nama_jenis_kamar, jk.harga_kamar, k.* FROM kamar AS k
-    LEFT JOIN jenis_kamar AS jk
-    ON jk.id = k.jenis_kamar
+    SELECT t.name AS type_name, t.price, r.* FROM rooms AS r
+    LEFT JOIN room_types AS t
+    ON t.id = r.room_type
     `;
 
   mysql.query(query, (error, result) => {
@@ -17,12 +17,12 @@ function getRooms(req, res) {
 function createRoom(req, res) {
   const values = [
     req.body.name,
-    req.body.jenis_kamar,
-    req.body.max_kapasitas
+    req.body.room_type,
+    req.body.max_capacity
   ];
   let query =
     `
-    INSERT INTO kamar (name, jenis_kamar, max_kapasitas) VALUES ?
+    INSERT INTO rooms (name, room_type, max_capacity) VALUES ?
     `;
 
   mysql.beginTransaction();
@@ -32,7 +32,7 @@ function createRoom(req, res) {
     const inventories = req.body.inventory.map((inv) => {
       return [ result.insertId, inv ];
     });
-    query = `INSERT INTO inventory_kamar (kamar_id, nama) VALUES ?`;
+    query = `INSERT INTO inventories (room_id, name) VALUES ?`;
 
     mysql.query(query, [inventories], (error, result) => {
       if (error) return res.status(400).send({ error });
@@ -46,7 +46,7 @@ function updateRoom(req, res) {
   const id = req.params.id;
   let query =
     `
-      SELECT * FROM kamar WHERE id = ?
+      SELECT * FROM rooms WHERE id = ?
     `;
 
   mysql.query(query, [id], (error, result) => {
@@ -56,13 +56,13 @@ function updateRoom(req, res) {
     const room = result[0];
     const values = [
       (req.body.name ?? room.name),
-      (req.body.max_kapasitas ?? room.max_kapasitas),
+      (req.body.max_capacity ?? room.max_capacity),
       (req.body.status ?? room.status),
       id
     ];
     query =
       `
-      UPDATE kamar SET name = ?, max_kapasitas = ?, status = ? WHERE id = ?
+      UPDATE rooms SET name = ?, max_capacity = ?, status = ? WHERE id = ?
       `;
 
     mysql.query(query, values, (error, result) => {
@@ -76,10 +76,11 @@ function deleteRoom(req, res) {
   const id = req.params.id;
   let query =
     `
-    DELETE FROM kamar WHERE id = ?
+    DELETE FROM inventories WHERE room_id = ?;
+    DELETE FROM rooms WHERE id = ?
     `;
 
-  mysql.query(query, [id], (error, result) => {
+  mysql.query(query, [id, id], (error, result) => {
     if (error) return res.status(400).send({ error });
     return res.status(200).send({ result });
   });
