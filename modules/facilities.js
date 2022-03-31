af1bcab782315ae94be15ac3db23b89eb81b5b9e
sglkc <31957516+sglkc@users.noreply.github.com>
2022-03-31 20:46:09 +0700

    feat: facilities endpoint and migration
    chore: update routes
    chore: adjust migrations

---
+++ modules/facilities.js
@@ -0,0 +1,90 @@
+const { mysql } = require('../db/connection.js');
+
+function getFacilities(req, res) {
+  let query =
+    `
+    SELECT * FROM facilities
+    `;
+
+  mysql.query(query, (error, result) => {
+    if (error) return res.status(400).send({ error });
+    return res.status(200).send({ result });
+  });
+}
+
+function createFacility(req, res) {
+  if (!Object.keys(req.body).length) {
+    return res.status(400).send({
+      error: 'name:string, notes:string|null'
+    });
+  }
+
+  const values = [
+    req.body.name,
+    req.body.notes
+  ];
+  let query =
+    `
+    INSERT INTO facilities (name, notes) VALUES ?
+    `;
+
+  mysql.query(query, [[values]], (error, result) => {
+    if (error) return res.status(400).send({ error });
+    return res.status(200).send({ result });
+  });
+}
+
+function updateFacility(req, res) {
+  if (!Object.keys(req.body).length) {
+    return res.status(400).send({
+      error: 'name:string, notes:string|null'
+    });
+  }
+
+  const id = req.params.id;
+  let query =
+    `
+      SELECT * FROM facilities WHERE id = ?
+    `;
+
+  mysql.query(query, [id], (error, result) => {
+    if (error) return res.status(400).send({ error });
+    if (!result.length) return res.status(400).send({ error: 'Not found' });
+
+    const facility = result[0];
+    const values = [
+      (req.body.name ?? facility.name),
+      (req.body.notes ?? facility.notes),
+      id
+    ];
+    query =
+      `
+      UPDATE facilities SET name = ?, notes = ? WHERE id = ?
+      `;
+
+    mysql.query(query, values, (error, result) => {
+      if (error) return res.status(400).send({ error });
+      return res.status(200).send({ result });
+    });
+  });
+}
+
+function deleteFacility(req, res) {
+  const id = req.params.id;
+  let query =
+    `
+    DELETE FROM facilities WHERE id = ?
+    `;
+
+  mysql.query(query, [id, id], (error, result) => {
+    if (error) return res.status(400).send({ error });
+    return res.status(200).send({ result });
+  });
+}
+
+module.exports = {
+  getFacilities,
+  createFacility,
+  updateFacility,
+  deleteFacility
+};
--- modules/routes.js
+++ modules/routes.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const { getRoles, login, register } = require('./auth.js');
-const { getRooms, createRoom, updateRoom, deleteRoom } = require('./rooms.js');
+const auth = require('./auth.js');
+const facilities = require('./facilities.js');
+const rooms = require('./rooms.js');
 const router = express.Router();
+let group = '';
 
-router.get('/auth/roles', getRoles);
-router.post('/auth/login', login);
-router.post('/auth/register', register);
+{
+  let group = '/auth';
+  router.get(group + '/roles', auth.getRoles);
+  router.post(group + '/login', auth.login);
+  router.post(group + '/register', auth.register);
+}
 
-router.get('/rooms', getRooms);
-router.post('/rooms', createRoom);
-router.put('/rooms/:id', updateRoom);
-router.delete('/rooms/:id', deleteRoom);
+{
+  let group = '/services';
+  router.get(group + '/facilities', facilities.getFacilities);
+  router.post(group + '/facilities', facilities.createFacility);
+  router.put(group + '/facilities/:id', facilities.updateFacility);
+  router.delete(group + '/facilities/:id', facilities.deleteFacility);
+}
+
+{
+  let group = '/services';
+  router.get(group + '/rooms', rooms.getRooms);
+  router.post(group + '/rooms', rooms.createRoom);
+  router.put(group + '/rooms/:id', rooms.updateRoom);
+  router.delete(group + '/rooms/:id', rooms.deleteRoom);
+}
 
 module.exports.routes = router;
