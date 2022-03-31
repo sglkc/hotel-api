ce32a983f89cc6986074db3f24ce99fe66298e76
sglkc <31957516+sglkc@users.noreply.github.com>
2022-03-31 18:57:22 +0700

    feat: authentication

---
+++ modules/auth.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { mysql } = require('../db/connection.js');
+
+function cryptPassword(password, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return callback(err);
+    bcrypt.hash(password, salt, (err, hash) => {
+      return callback(err, hash);
+    });
+  });
+}
+
+function register(req, res) {
+  cryptPassword(req.body.password, (error, hash) => {
+    if (error) return res.status(400).send({ error });
+
+    mysql.query(
+      'SELECT * FROM users WHERE EMAIL IN (?)',
+      [ req.body.email ],
+      (error, result) => {
+        if (error) return res.status(400).send({ error });
+        if (result.length) {
+          return res.status(400).send({ error: 'Email has been registered' });
+        }
+      }
+    );
+
+    mysql.query(
+      'INSERT INTO users (fullname, email, password, role) VALUES (?)',
+      [ [ req.body.fullname, req.body.email, hash, req.body.role ] ],
+      (error, result) => {
+        if (error) return res.status(400).send({ error });
+        return res.status(200).send({ result });
+      }
+    );
+  });
+}
+
+function login(req, res) {
+  mysql.query(
+    'SELECT * FROM users WHERE email = ?',
+    [ req.body.email ],
+    (error, result) => {
+      if (error) return res.status(400).send({ error });
+      if (!result.length) {
+        return res.status(400).send({ error: 'Email do not match our record' });
+      }
+
+      const user = result[0];
+      bcrypt.compare(req.body.password, user.password, (error, result) => {
+        if (error) return res.status(400).send({ error });
+        if (!res) return res.status(400).send({ error: 'Password is invalid'});
+
+        const token = jwt.sign(
+          { id: user.id }, process.env.JWT_SECRET, { expiresIn: '2h' }
+        );
+
+        return res.status(200).send({ result: { token, user } });
+      });
+    }
+  )
+}
+
+module.exports = { login, register };
--- modules/routes.js
+++ modules/routes.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const { login, register } = require('./auth.js');
 const { getRooms, createRoom, updateRoom, deleteRoom } = require('./rooms.js');
 const router = express.Router();
+
+router.post('/auth/login', login);
+router.post('/auth/register', register);
 
 router.get('/rooms', getRooms);
 router.post('/rooms', createRoom);
